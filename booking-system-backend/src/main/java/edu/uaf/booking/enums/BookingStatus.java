dbab3664520af21f51aef7f0aafb2c07b07a8b15
vuphm <vuphamletruong@gmail.com>
2026-05-16 19:24:31 +0700

    chore: clear raw files to prepare for submodule integration

--- booking-system-backend/src/main/java/edu/uaf/booking/enums/BookingStatus.java
+++
@@ -1,8 +0,0 @@
-package edu.uaf.booking.enums;
-
-public enum BookingStatus {
-    PENDING,    // Chờ xử lý
-    CONFIRMED,  // Đã xác nhận lịch
-    COMPLETED,  // Đã hoàn thành liệu trình
-    CANCELLED   // Đã hủy lịch
-}
--- booking-system-backend/src/main/java/edu/uaf/booking/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package edu.uaf.booking.enums;
-
-public enum Role {
-    PROVIDER_ADMIN,
-    CUSTOMER
-}
